7ccdc2177192f973516faed817090783cf662a73
ivanreyess <melvre14@gmail.com>
2023-06-02 14:47:26 -0600

    feat: adding user commons service

--- user-service/src/main/java/com/sv/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/sv/userservice/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.sv.userservice.repository;
 
-import com.sv.userservice.domain.User;
+import com.sv.usercommonsservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
