32f6ba02306da0919473a2d7af3692aed961cb8e
ivanreyess <melvre14@gmail.com>
2023-06-02 17:27:08 -0600

    feat: configuring security

---
+++ gateway-service/src/main/java/com/sv/gatewayservice/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.sv.gatewayservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/api/security/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/productos/listar",
+                        "/api/items/listar",
+                        "/api/usuarios/usuarios",
+                        "/api/items/ver/{id}/cantidad/{cantidad}",
+                        "/api/productos/ver/{id}").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+                .pathMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/usuarios/**").hasRole("ADMIN")
+                .anyExchange().authenticated()
+                .and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable()
+                .build();
+    }
+
+
+}
---
+++ oauth-service/src/main/java/com/sv/service/oauth/security/AuthorizationServerConfig.java
@@ -0,0 +1,2 @@
+package com.sv.service.oauth.security;public class AuthorizationServerConfig {
+}
